b2210267efaa3bf1344b30186a26e3e252057daa
JohariSahil <sahiljohari04@gmail.com>
2024-06-16 18:09:08 +0530

    added api for section

--- src/components/section/sectionForm/SectionForm.jsx
+++ src/components/section/sectionForm/SectionForm.jsx
@@ -4,14 +4,42 @@ import { Form } from "react-router-dom";
 const SectionForm = () => {
   const [sectionName, setSectionName] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     if (sectionName.trim() === "") {
       setErrorMessage("This field is required");
     } else {
       setErrorMessage("");
-      // Submit the form or handle successful submission
+      // Submit the form and handling successful submission
+      setSuccessMessage("");
+
+      try {
+        const response = await fetch(
+          "http://localhost:3000/section/add-section",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ section: sectionName }),
+          }
+        );
+
+        if (response.ok) {
+          const result = await response.json();
+          setSuccessMessage("Section created successfully!");
+          setSectionName(""); // Clear the form
+        } else {
+          const errorData = await response.json();
+          setErrorMessage(
+            errorData.message || "An error occurred. Please try again."
+          );
+        }
+      } catch (error) {
+        setErrorMessage("An error occurred. Please try again.");
+      }
     }
   };
 
