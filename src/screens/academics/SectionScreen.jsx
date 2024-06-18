334813793bbb50d63b4dd3c19656a97246747ce2
JohariSahil <sahiljohari04@gmail.com>
2024-06-19 00:43:55 +0530

    added fix for newly created section

--- src/screens/academics/SectionScreen.jsx
+++ src/screens/academics/SectionScreen.jsx
@@ -50,24 +50,16 @@ const Section = () => {
           fetch("http://localhost:3000/section/get-sections")
             .then((response) => response.json())
             .then((data) => {
-              console.log("hjbsibfawf", data);
               setProducts((prevProducts) => [
                 ...prevProducts,
                 {
-                  section_id: result.section_id,
+                  section_id:
+                    data.sections[data.sections.length - 1].section_id,
                   serialNumber: prevProducts.length + 1,
-                  section: sectionName,
+                  section: data.sections[data.sections.length - 1].section_name,
                 },
               ]);
             });
-          // setProducts((prevProducts) => [
-          //   ...prevProducts,
-          //   {
-          //     section_id: result.section_id,
-          //     serialNumber: prevProducts.length + 1,
-          //     section: sectionName,
-          //   },
-          // ]);
           toast.success("Section created successfully!");
         } else {
           toast.error(result.message || "An error occurred. Please try again.");
