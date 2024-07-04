48d3e654a61ef6a7db951cdef20551acf1030d8c
JohariSahil <sahiljohari04@gmail.com>
2024-07-04 23:16:19 +0530

    added teacher form

--- src/components/teacher/teacherForm/TeacherForm.jsx
+++ src/components/teacher/teacherForm/TeacherForm.jsx
@@ -1,5 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Form } from "react-router-dom";
+
 const TeacherForm = () => {
-  return <div>Sahil</div>;
+  return (
+    <Form
+      className="section-form"
+      // encType="multipart/form-data"
+      // onSubmit={handleSubmit}
+    >
+      <div className="form-top-title">Create Teacher</div>
+      <div className="form-area">
+        <div className="form-second-title">Name</div>
+        <div className="form-textarea">
+          <textarea
+            placeholder="Class Name"
+            // value={name}
+            // onChange={(e) => setName(e.target.value)}
+          ></textarea>
+          {/* {nameError && <div className="error-message">{nameError}</div>} */}
+        </div>
+      </div>
+      <div className="form-area">
+        <div className="form-second-title">Section</div>
+        <div className="form-textarea">
+          {/* {sectionError && <div className="error-message">{sectionError}</div>} */}
+        </div>
+      </div>
+      <div className="form-area">
+        <div className="form-title">
+          Stream
+          <span style={{ color: "var(--primary-color)", marginLeft: "5px" }}>
+            (optional)
+          </span>
+        </div>
+        <div className="form-textarea"></div>
+      </div>
+      <button type="submit" className="submit-btn">
+        Submit
+      </button>
+    </Form>
+  );
 };
 
 export default TeacherForm;
